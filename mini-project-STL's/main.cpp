9a6582695b35d455c3d9d66cc4528d1eac02e6cc
mohamed <mr.mohamedbouharti@gmail.com>
2025-10-03 09:10:17 +0100

    new feature: grouping students randomly.

--- mini-project-STL's/main.cpp
+++ mini-project-STL's/main.cpp
@@ -20,30 +20,30 @@ struct Student
     }
 };
 
-struct Time
-{
-    using steady_clock = std::chrono::steady_clock;
-    using duration = std::chrono::duration<double, std::micro>;
-    std::string name_;
-    steady_clock::time_point tp_;
+// struct Time
+// {
+//     using steady_clock = std::chrono::steady_clock;
+//     using duration = std::chrono::duration<double, std::micro>;
+//     std::string name_;
+//     steady_clock::time_point tp_;
 
-    Time(const std::string &st)
-    {
-        name_ = st;
-        tp_ = steady_clock::now();
-    };
+//     Time(const std::string &st)
+//     {
+//         name_ = st;
+//         tp_ = steady_clock::now();
+//     };
 
-    ~Time()
-    {
-        auto elapsed = steady_clock::now();
-        std::cout << "time of '" << name_ << "': "
-                  << duration(elapsed - tp_).count()
-                  << "μs\n";
-    };
-};
+//     ~Time()
+//     {
+//         auto elapsed = steady_clock::now();
+//         std::cout << "time of '" << name_ << "': "
+//                   << duration(elapsed - tp_).count()
+//                   << "μs\n";
+//     };
+// };
 
-constexpr int min_srore = 0;
-constexpr int max_srore = 100;
+constexpr int min_score = 0;
+constexpr int max_score = 100;
 
 struct Random
 {
@@ -51,7 +51,7 @@ struct Random
         std::random_device{}()};
 
     std::uniform_int_distribution<> distrib_{
-        min_srore, max_srore};
+        min_score, max_score};
 
     Random() = default;
 
@@ -75,7 +75,7 @@ int main()
         constexpr int students_number = 100;
 
         Random ran;
-        for (size_t i = 0; i < students_number; ++i)
+        for (size_t i = 1; i <= students_number; ++i)
         {
             students_.emplace_back("s" + std::to_string(i),
                                    ran.random_score());
@@ -88,7 +88,7 @@ int main()
         constexpr int bonus = 10;
         constexpr int cap_score = 100;
 
-        Time time("transform");
+        // Time time("transform");
         std::transform(students_.begin(), students_.end(),
                        std::back_inserter(students_after_bonus_),
 
@@ -102,7 +102,7 @@ int main()
 
     // Adding +1 pointe to Alice
     {
-        Time time("find_if");
+        // // Time time("find_if");
         auto it = std::find_if(students_after_bonus_.begin(),
                                students_after_bonus_.end(),
                                [](const Student &student)
@@ -115,18 +115,18 @@ int main()
     double average;
     {
         int init = 0;
-        Time time("accumulate");
+        // // Time time("accumulate");
         int total = std::accumulate(students_after_bonus_.begin(),
                                     students_after_bonus_.end(),
-                                    init, [](int i, const Student &student)
-                                    { return i + student.score_; });
+                                    init, [](int n, const Student &student)
+                                    { return n + student.score_; });
         average = static_cast<double>(total) / students_after_bonus_.size();
     }
 
     std::vector<Student> above_average_students_;
     std::vector<Student> below_average_students_;
     {
-        Time time("partition_copy");
+        // // Time time("partition_copy");
         std::partition_copy(students_after_bonus_.begin(),
                             students_after_bonus_.end(),
                             std::back_inserter(above_average_students_),
@@ -144,20 +144,89 @@ int main()
                   { return a.score_ > b.score_; });
     };
     {
-        Time time("sort");
+        // // Time time("sort");
         sort_the(above_average_students_);
         sort_the(below_average_students_);
     }
 
+    // Remove failing students
+    {
+        constexpr int bottom_Borderline = 50;
+        // // Time time("erase-remove_if:");
+        below_average_students_.erase(
+            std::remove_if(below_average_students_.begin(),
+                           below_average_students_.end(),
+                           [](const Student &student)
+                           { return student.score_ < bottom_Borderline; }),
+            below_average_students_.end());
+    }
     // Partition
     std::vector<Student>::iterator mid;
     {
-        Time time("stable_partition");
+        constexpr int cap_Borderline = 80;
+        // // Time time("stable_partition");
         mid = std::stable_partition(above_average_students_.begin(),
                                     above_average_students_.end(),
                                     [](const Student &student)
-                                    { return student.score_ > 70; });
+                                    { return student.score_ > cap_Borderline; });
     }
+
+    // Next semester groups
+
+    std::vector<Student> passed_students_ =
+        above_average_students_;
+
+    std::copy(below_average_students_.begin(),
+              below_average_students_.end(),
+              std::back_inserter(passed_students_));
+
+    using students_pair_vec = std::vector<std::pair<Student, Student>>;
+
+    students_pair_vec students_groups_;
+    {
+        Random ran;
+        // // Time time("Shuffle");
+        std::shuffle(passed_students_.begin(),
+                     passed_students_.end(), ran.gen_);
+
+        bool is_student_solo = false;
+        Student solo_student{};
+
+        if (passed_students_.size() % 2 != 0)
+        {
+            solo_student = std::move(passed_students_.back());
+            passed_students_.pop_back();
+            is_student_solo = true;
+        }
+        for (auto it = passed_students_.begin();
+             it != passed_students_.end(); it += 2)
+        {
+            students_groups_.emplace_back(*it, *(it + 1));
+        }
+        if (is_student_solo)
+            students_groups_.push_back({solo_student, {}});
+    }
+
+    auto print_students2 =
+        [](students_pair_vec::iterator begin_,
+           students_pair_vec::iterator end_,
+           const std::string &ui)
+    {
+        if (begin_ == end_)
+            return;
+        int num = 1;
+        std::cout
+            << ui << "\n";
+        auto it = begin_;
+        for (; it != end_ - 1; ++it)
+        {
+            std::cout << "#" << num++ << "\n-" << it->first << "\n"
+                      << "-" << it->second << "\n";
+        }
+        std::cout << "#" << num++ << "\n-" << (it)->first << "\n";
+        if (!it->second.name_.empty())
+            std::cout << "-" << it->second << "\n";
+    };
 
     auto print_students =
         [](std::vector<Student>::iterator begin_,
@@ -170,23 +239,29 @@ int main()
             std::cout << *it << "\n";
     };
 
+    std::cout << "Class average: "
+              << average << "\n";
+    std::cout << "\n";
+
+    std::cout << "Passing students:\n";
     print_students(above_average_students_.begin(),
                    mid,
-                   "Passing students:");
+                   "->Toppers students:");
     std::cout << "\n";
 
     print_students(mid,
                    above_average_students_.end(),
-                   "Borderline students:");
+                   "->Borderline students:");
     std::cout << "\n";
 
     print_students(below_average_students_.begin(),
                    below_average_students_.end(),
-                   "below average students:");
+                   "->below avg students:");
     std::cout << "\n";
 
-    std::cout << "Class average: "
-              << average << "\n";
+    print_students2(students_groups_.begin(),
+                    students_groups_.end(),
+                    "Students groups:");
 
     return 0;
 }
